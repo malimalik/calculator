c4fcb05ddb0fcb26784e781157e162fb9d318faa
Ali <ialimalik99@gmail.com>
2023-11-27 13:16:18 -0500

    add initial interest

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import Header from "./components/Header";
-import React from "react";
+import React, { useState, Fragment } from "react";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
 
 function App() {
-  return <Header />;
+  const [data, setData] = useState({
+    initialInvestment: 0,
+    annualInvestment: 0,
+    expectedReturn: 0,
+    duration: 0,
+  });
+
+  const handleChange = (e) => {
+    setData((prevData) => {
+      return {
+        ...prevData,
+        [e.target.name]: +e.target.value,
+      };
+    });
+  };
+  return (
+    <Fragment>
+      <Header />
+      <UserInput handleChange={handleChange} />
+      <Results data={data} />
+    </Fragment>
+  );
 }
 
 export default App;
